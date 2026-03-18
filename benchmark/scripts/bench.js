47f2d0a85ab399b4faac634c85c50722f373ecd0
BizWiz3 <bizwiz300@gmail.com>
2026-03-18 19:24:46 +0800

    bench test: 1.5k VU

--- benchmark/scripts/bench.js
+++ benchmark/scripts/bench.js
@@ -4,7 +4,7 @@ import { check } from 'k6';
 export const options = {
   stages: [
     { duration: '10s', target: 200 },
-    { duration: '30s', target: 2000 },
+    { duration: '30s', target: 1500 },
     { duration: '10s', target: 0 },
   ],
 };
